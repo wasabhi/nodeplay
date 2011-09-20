03d4d7d6a31e8470abb7ca1fe069181314d0a4d3
wasabhi <abhinav.keswani@trineo.co.nz>
2011-09-20 16:03:59 +1200

    Request handlers in the router

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
-function route(pathname) {
+function route(handle, pathname) {
   console.log("About to route " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname]();
+  }else{
+    console.log("No request handler found for " + pathname);
+  }
 };
 
 exports.route = route;
