e49b67f369d54294b632c40fb71376b65a937b2f
wasabhi <abhinav.keswani@trineo.co.nz>
2011-10-02 15:51:20 +1300

    Bug fixes in router. 1st cut non-blocking callback

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("request for " + pathname + " received");
 
-    route(handle, pathname);
-
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    var content = route(handle, pathname)
+    response.write(content);
     response.end();
-  };
+  }
 
   http.createServer(onRequest).listen(8888);
   console.log("started server");
