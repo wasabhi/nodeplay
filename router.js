cba53bccf466ce4ce3ae60c5389d39e800c5bc4f
wasabhi <abhinav.keswani@trineo.co.nz>
2011-10-02 16:01:12 +1300

    Non blocking passing response object

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   console.log("About to route " + pathname);
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname]();
+    return handle[pathname](response);
   }else{
     console.log("No request handler found for " + pathname);
-    return "404 Not Found";
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
   }
 };
 
